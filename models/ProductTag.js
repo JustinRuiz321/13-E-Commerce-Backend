de30ebc271e890d3a97d93979191f26818e4e3d9
JustinRuiz321 <107062112+JustinRuiz321@users.noreply.github.com>
2022-11-07 16:07:31 -0500

    product tag columns defined

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,27 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id : {
+      type : DataTypes.INTEGER,
+      primaryKey : true ,
+      autoIncrement : true ,
+      allowNull : false ,
+    },
+    tagID : {
+      type : DataTypes.INTEGER,
+      references : {
+        model : 'tag' ,
+        key : 'id' 
+      }
+    },
+    productID : {
+      type : DataTypes.INTEGER ,
+      references : {
+        model : 'product' ,
+        key : 'id' 
+      }
+    },
+    
   },
   {
     sequelize,
