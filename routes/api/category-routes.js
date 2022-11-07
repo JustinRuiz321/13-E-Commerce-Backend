6fae247d4af8e9fadd5d570a0e3801487181158e
JustinRuiz321 <107062112+JustinRuiz321@users.noreply.github.com>
2022-11-07 15:48:05 -0500

    cat routes, finished api folder code. Need to work on models

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,81 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const catInfo = await Category.findAll ({
+      include: [{model: Product}]
+    });
+    res.status(200).json(catInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const catInfo = await Category.findByPk( req.params.id, {
+      include: [{model: Product}]
+    });
+    if (!catInfo) {
+      res.status(404).json({message: 'ID not found'});
+      return;
+    }
+    res.status(200).json(catInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const catInfo = await Category.create({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(catInfo);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try{
+    const catInfo = await Category.update(req.body , {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!catInfo) {
+      res.status(404).json({message: 'ID not found'});
+      return;
+    }
+    res.status(200).json(catInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const catInfo = await Category.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!catInfo) {
+      res.status(404).json({message: 'ID not found'});
+      return;
+    }
+    res.status(200).json(catInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
